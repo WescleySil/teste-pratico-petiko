91f1edafc7bc78537070fdaa3fce3ccd64df1a0f
WescleySil <wescley.silva.cas@gmail.com>
2025-07-25 20:50:49 -0300

    funciona por favor

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,12 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
-        $middleware->validateCsrfTokens(except: [
-            'api/*',
-        ]);
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {})->create();
--- config/cors.php
+++ config/cors.php
@@ -1,48 +1,11 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-        'teste-pratico-petiko-front.onrender.com'
-    ],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => [
-        'Accept',
-        'Authorization',
-        'Content-Type',
-        'X-Requested-With',
-        'X-CSRF-TOKEN',
-    ],
-
-    'exposed_headers' => [],
-
-    'max_age' => 86400,
-
+    'allowed_methods' => ['GET','POST','PUT','DELETE','OPTIONS'],
+    'allowed_origins' => ['teste-pratico-petiko-front.onrender.com'],
+    'allowed_headers' => ['*'],
     'supports_credentials' => true,
-
+    'max_age' => 86400,
 ];
+
